605549caf9e5480f06a233f1be149313e879fc60
Katarina Yang <yangkb09@gmail.com>
2021-05-30 17:17:07 -0400

    array: twoNumberSum pointer solution

--- Array/Easy/twoNumberSum.js
+++ Array/Easy/twoNumberSum.js
@@ -1,8 +1,33 @@
 //https://www.algoexpert.io/questions/Two%20Number%20Sum
 
+// ** SOLUTION THREE **
+//pointers
 
+// time & space complexity:
+  // time: o(nlog(n))
+  // space: o(1)
 
-// SOLUTION TWO
+function twoNumberSum(array, targetSum) {
+	array.sort((a, b) => a - b)
+
+	let leftPointer = 0;
+	let rightPointer = array.length - 1;
+
+	while (leftPointer < rightPointer) {
+		const currentSum = array[leftPointer] + array[rightPointer];
+		if (currentSum === targetSum) {
+			return [array[leftPointer], array[rightPointer]]
+		} else if (currentSum < targetSum) {
+			leftPointer++;
+		} else if (currentSum > targetSum) {
+			rightPointer--;
+		}
+	}
+	return [];
+}
+
+// ** SOLUTION TWO **
+// hash table
 
 // time & space complexity:
   // o(n) for both, where n is length of array
@@ -21,7 +46,19 @@ function twoNumberSum(array, targetSum) {
 }
 
 
-// SOLUTION ONE
+// ** SOLUTION ONE **
+
+function twoNumberSum(array, targetSum) {
+	let hashTable = {};
+	for (let i = 0; i < array.length; i++) {
+		let curNum = array[i];
+		hashTable[curNum] = 1;
+		if (hashTable[targetSum - curNum] && (targetSum - curNum !== curNum)) {
+			return [curNum, targetSum - curNum]
+		}
+	}
+	return [];
+}
 
 // pseudocode
   //edge cases:
@@ -36,16 +73,4 @@ function twoNumberSum(array, targetSum) {
 
 // time & space complexity:
   // o(n) for both, where n is length of array
-
-function twoNumberSum(array, targetSum) {
-	let hashTable = {};
-	for (let i = 0; i < array.length; i++) {
-		let curNum = array[i];
-		hashTable[curNum] = 1;
-		if (hashTable[targetSum - curNum] && (targetSum - curNum !== curNum)) {
-			return [curNum, targetSum - curNum]
-		}
-	}
-	return [];
-}
 
