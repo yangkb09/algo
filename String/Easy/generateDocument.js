75199f880b5b93c7d26f900fab298db558c33d2a
Katarina Yang <yangkb09@gmail.com>
2021-06-14 17:54:45 -0400

    string, easy: set up generate document

---
+++ String/Easy/generateDocument.js
@@ -0,0 +1,31 @@
+//https://www.algoexpert.io/questions/Generate%20Document
+
+// ** SOLUTION ONE **
+//
+
+// TIME & SPACE
+  // time:
+  // space:
+
+// CODE
+
+
+// APPROACH
+  /*
+    input: ; output:
+
+    examples:
+
+
+    edge cases:
+
+    brute force:
+
+    optimized:
+
+  */
+
+// NOTES
+  /*
+
+  */
